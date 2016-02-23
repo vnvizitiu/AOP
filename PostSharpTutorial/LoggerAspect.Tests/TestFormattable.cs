ae62300634164fdd155a5c731629cbe58bfaffa5
ignatandrei <ignatandrei@yahoo.com>
2016-02-23 06:26:35 +0200

    added test for FormattableObject

---
+++ PostSharpTutorial/LoggerAspect.Tests/TestFormattable.cs
@@ -0,0 +1,20 @@
+﻿using LoggerAspect;
+using NUnit.Framework;
+
+namespace LoggerAspect.Tests
+{
+    [TestFixture]
+    public class TestFormattable
+    {
+        [Test]
+        public void TestWithProperties()
+        {
+            string site = "http://msprogrammer.serviciipeweb.ro/";
+            var p = new Person();
+            p.Website = site;
+            var str = p.ToString("{Website}");
+            Assert.AreEqual(str, site);
+
+        }
+    }
+}
